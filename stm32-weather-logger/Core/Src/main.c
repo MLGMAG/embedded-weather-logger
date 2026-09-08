aff1434c7d96e86a5f69a6530b88b6bd85ddf25a
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2026-09-08 13:24:03 +0300

    [ProjectSTM32] Change architecture to STM32F411

--- stm32-weather-logger/Core/Src/main.c
+++ stm32-weather-logger/Core/Src/main.c
@@ -111,7 +111,7 @@ void SystemClock_Config(void)
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
